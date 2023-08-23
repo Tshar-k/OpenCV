612f5d29a4c59bc864671ed8ad1751e2cbe55b54
Tushar Kumar <117516567+Tshar-k@users.noreply.github.com>
2023-08-23 18:48:18 +0530

    Add files via upload

---
+++ Adaptive Threshold.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('smarties.png',0)
+
+th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2);
+th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2);
+
+
+cv.imshow("ADAPTIVE_THRESH_MEAN_C", th2)
+cv.imshow("ADAPTIVE_THRESH_GAUSSIAN_C", th3)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
